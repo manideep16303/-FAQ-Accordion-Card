91394be3610082aa07c74cf5227fae5b26f6e15b
manideep16303 <manideep16303@gmail.com>
2021-05-27 22:12:42 +0530

    faq page

---
+++ index.js
@@ -0,0 +1,24 @@
+$(".arrow-1, .h-1").click(function () {
+  $(".p-1").slideToggle(210, 0);
+  $(".p-1").css("display", "block");
+});
+$(".arrow-2, .h-2").click(function () {
+  $(".p-2").slideToggle(210, 0);
+  $(".p-2").css("display", "block");
+});
+$(".arrow-3, .h-3").click(function () {
+  $(".p-3").slideToggle(210, 0);
+  $(".p-3").css("display", "block");
+});
+$(".arrow-4, .h-4").click(function () {
+  $(".p-4").slideToggle(210, 0);
+  $(".p-4").css("display", "block");
+});
+$(".arrow-5, .h-5").click(function () {
+  $(".p-5").slideToggle(210, 0);
+  $(".p-5").css("display", "block");
+});
+
+$("h4").click(function () {
+  $(".box-img").css("margin-left", "-500px");
+});
